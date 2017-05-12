a733e4ebbcca322fa3913e89aafac8efed7c2167
Susha Dore <susha.dore@gmail.com>
2017-05-12 16:43:15 -0700

    pass Brand spec ensuring database price column is of money type

--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,5 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many(:brands)
 
+
 end
--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -2,4 +2,5 @@ require "spec_helper"
 
 describe Brand do
   it { should have_and_belong_to_many :stores}
+  it { should have_db_column(:price).of_type(:money)}
 end
