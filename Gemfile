c4c855ab380dc722807d500e505176b620470747
Susha Dore <susha.dore@gmail.com>
2017-05-12 15:38:23 -0700

    set up necessary files for shoe_store project including Sinatra, Activerecord, Rake and Postgres

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source "https://rubygems.org"
+
+gem "pg"
+gem "sinatra"
+gem "sinatra-contrib", :require => "sinatra/reloader"
+gem "sinatra-activerecord"
+gem "rake"
+gem "pry"
+
+
+group :test do
+  gem "capybara"
+  gem "rspec"
+  gem "shoulda-matchers", '~> 2.0'
+end
---
+++ db/migrate/20170512212058_create_tables.rb
@@ -0,0 +1,18 @@
+class CreateTables < ActiveRecord::Migration[5.1]
+  def change
+    create_table :brands do |t|
+      t.column(:brand_name, :string)
+      t.column(:price, :money)
+      t.timestamps
+    end
+    create_table :brands_stores do |t|
+      t.column(:brand_id, :integer)
+      t.column(:store_id, :integer)
+      t.timestamps
+    end
+    create_table :stores do |t|
+      t.column(:store_name, :varchar)
+      t.timestamps
+    end
+  end
+end
