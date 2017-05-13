f767bbc409efbff674c5c6f5409d573ff2fc18f0
Susha Dore <susha.dore@gmail.com>
2017-05-12 17:37:50 -0700

    validates uniqueness of brand_name input, passing spec

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,6 +1,7 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many :stores
   validates :brand_name, presence: true
-  # length: { maximum: 100, too_long: "%{count} characters maximum"}
+  validates :brand_name, uniqueness: true 
+    # length: { maximum: 100, too_long: "%{count} characters maximum"}
 
 end
--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -4,4 +4,5 @@ describe Brand do
   it { should have_and_belong_to_many :stores }
   it { should have_db_column(:price).of_type :money }
   it { should validate_presence_of(:brand_name) }
+  it { should validate_uniqueness_of(:brand_name)}
 end
