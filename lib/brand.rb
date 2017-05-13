cb9275ef4c172289235be8df719b07b36ecaa2e3
Susha Dore <susha.dore@gmail.com>
2017-05-12 17:20:15 -0700

    validate presence brand_name input, passing spec

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,3 +1,6 @@
 class Brand < ActiveRecord::Base
-  has_and_belongs_to_many(:stores)
+  has_and_belongs_to_many :stores
+  validates :brand_name, presence: true
+  # length: { maximum: 100, too_long: "%{count} characters maximum"}
+
 end
--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -1,6 +1,7 @@
 require "spec_helper"
 
 describe Brand do
-  it { should have_and_belong_to_many :stores}
-  it { should have_db_column(:price).of_type(:money)}
+  it { should have_and_belong_to_many :stores }
+  it { should have_db_column(:price).of_type :money }
+  it { should validate_presence_of(:brand_name) }
 end
