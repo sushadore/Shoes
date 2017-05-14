fd83c54db814f582beecd84b6615847da2c33ae6
Susha Dore <susha.dore@gmail.com>
2017-05-14 11:45:05 -0700

    requires unique store name, passing spec

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -3,6 +3,6 @@ require "spec_helper"
 describe Store do
   it { should have_and_belong_to_many :brands}
   it { should validate_presence_of :store }
-  # it { should validate_uniqueness_of :store }
+  it { should validate_uniqueness_of :store }
   # it { should validate_length_of :store }
 end
