04aa95ec005fe27f9d37edd95523ea84037e5039
Susha Dore <susha.dore@gmail.com>
2017-05-14 13:26:52 -0700

    allow user to delete store from database, passes store_pages spec

--- spec/store_pages_spec.rb
+++ spec/store_pages_spec.rb
@@ -13,9 +13,27 @@ end
 describe "updating a store name", :type => :feature do
   it "allows user to update store name" do
     visit "/"
+    click_link "Add Store"
+    fill_in "store", with: "susha's shoes"
+    click_button "Add Store"
+    expect(page).to have_content "Susha's Shoes"
     click_link "Susha's Shoes"
     fill_in "store", with: "Sushalou's"
     click_button "Edit"
     expect(page).to have_content "Sushalou's"
+  end
+end
+
+describe "deleting a store", :type => :feature do
+  it "allows user to delete a store" do
+    visit "/"
+    click_link "Add Store"
+    fill_in "store", with: "susha's shoes"
+    click_button "Add Store"
+    expect(page).to have_content "Susha's Shoes"
+    click_link "Susha's Shoes"
+    expect(page).to have_content "Susha's Shoes"
+    click_button "Delete Store"
+    expect(page).to have_content " "
   end
 end
